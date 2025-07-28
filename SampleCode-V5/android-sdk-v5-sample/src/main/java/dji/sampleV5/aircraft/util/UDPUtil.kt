33b61634c26da9d302d0f04dbdef461a706b90e2
Equisetum-sp <joker.chpy.2@gmail.com>
2025-07-28 16:53:16 +0700

    Change flight data to JSON

--- SampleCode-V5/android-sdk-v5-sample/src/main/java/dji/sampleV5/aircraft/util/UDPUtil.kt
+++ SampleCode-V5/android-sdk-v5-sample/src/main/java/dji/sampleV5/aircraft/util/UDPUtil.kt
@@ -1,18 +1,13 @@
 package dji.sampleV5.aircraft.util
 
 import dji.sampleV5.aircraft.models.FlightData
+import org.json.JSONObject
 import java.net.DatagramPacket
 import java.net.DatagramSocket
 import java.net.InetAddress
 import java.net.InetSocketAddress
-import java.nio.ByteBuffer
-import java.util.concurrent.Executors
-import java.util.concurrent.ScheduledExecutorService
-import java.util.concurrent.TimeUnit
 
 class UDPUtil {
-    val dataStreamScheduler= Executors.newSingleThreadScheduledExecutor()
-
     private var socket = DatagramSocket(null)
     private var udpIPAddr : InetAddress = InetAddress.getByName("")
     private val udpPort = 10004
@@ -34,47 +29,24 @@ class UDPUtil {
         socket.close()
     }
 
-    fun startSendFlightData(streamStartTimeMs: Long, interval: Long = 80){
-        //Start thread, periodically send flight data (and other stuff) using UDP
-//        val dataStreamScheduler= Executors.newSingleThreadScheduledExecutor()
-        dataStreamScheduler.scheduleWithFixedDelay({
-            val pts = (System.currentTimeMillis() - streamStartTimeMs)
-            sendFlightData(pts)
-        }, 0, interval, TimeUnit.MILLISECONDS)
-    }
-
-    fun stopSendFlightData(){
-        dataStreamScheduler.shutdown()
-    }
-
     fun sendFlightData(currPTS: Long){
-        val flightDataHeader = createUDPHeader(currPTS)
-        val flightDataBody = createUDPFlightDataBody()
-        val flightDataPacket = flightDataHeader + flightDataBody
+        val flightDataPacket = createUDPFlightDataBody(currPTS)
 
         val packet = DatagramPacket(flightDataPacket, flightDataPacket.size, udpIPAddr, udpPort)
         socket.send(packet)
     }
 
-    private fun createUDPHeader(presentationTimeMs: Long): ByteArray{
-        return ByteBuffer.allocate(Long.SIZE_BYTES).putLong(presentationTimeMs).array()
-    }
+    private fun createUDPFlightDataBody(presentationTimeMs: Long): ByteArray{
+        val json = JSONObject()
 
-    private fun createUDPFlightDataBody(): ByteArray{
-        val buffer = ByteBuffer.allocate(16)
+        json.put("pts", presentationTimeMs)
+        json.put("battery", FlightData.batteryPercent)
+        json.put("gpsCount", FlightData.gpsCount)
+        json.put("alt", FlightData.altitude)
+        json.put("lat", FlightData.location.latitude)
+        json.put("lng", FlightData.location.longitude)
+        json.put("facing", FlightData.facing)
 
-        buffer.put(FlightData.batteryPercent.toByte())
-
-        buffer.put(FlightData.gpsCount.toByte())
-
-        buffer.putInt((FlightData.altitude * 1000).toInt())
-
-        buffer.putInt((FlightData.location.latitude * 1000000).toInt())
-
-        buffer.putInt((FlightData.location.longitude * 1000000).toInt())
-
-        buffer.putShort((FlightData.facing * 10).toInt().toShort())
-
-        return buffer.array()
+        return json.toString().toByteArray(Charsets.UTF_8)
     }
 }
